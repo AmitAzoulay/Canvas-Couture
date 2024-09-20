e7ce4720698bd9fc4459a551eec9f9cd8798f0b2
עמית אזולאי <amitazoulay1611@gmail.com>
2024-09-20 18:36:37 +0300

    added html and css to the main page

---
+++ controllers/products.js
@@ -0,0 +1,7 @@
+function getProductByCategory(req, res) {
+    res.render('index.ejs')
+}
+
+module.exports = {
+    getProductByCategory
+}
--- main.js
+++ main.js
@@ -2,33 +2,19 @@ const mongoose = require('mongoose');
 const express = require('express')
 var cors = require('cors')
 require('dotenv').config();
-const ordersRoute = require('./routes/order');
-const productsRoute = require('./routes/product');
+const productController = require('./controllers/products')
 
-const port = +process.env.PORT
+const server = express()
+server.use(cors())
+server.use(express.static('public'))
+
+server.get("/products", productController.getProductByCategory)
+
+server.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
 
 mongoose.connect(process.env.DB_URI)
     .then(() => console.log('Connected!'));
-
-
-const app = express()
-app.use(cors())
-app.use('/routes/order', ordersRoute);
-app.use('/routes/product', productsRoute);
-
-/*app.get('/', async (req, res) => {
-    try {
-        const users = await Users.find()
-        const products = await Products.find()
-        res.json({ users, products, orders });
-    }
-    catch (error) {
-        res.status(500).send('Error fetching data');
-    }
-})*/
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
 
 
