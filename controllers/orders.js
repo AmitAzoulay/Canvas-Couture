9ee68971c126b3fee3ca2c9f95731a9a940a1a2c
עמית אזולאי <amitazoulay1611@gmail.com>
2024-09-23 22:09:06 +0300

    fixed bug in user's orders history after merging

--- controllers/orders.js
+++ controllers/orders.js
@@ -11,23 +11,6 @@ async function getAllOrdersOfUser_controller(req, res) {
             console.error('Error fetching products:', error);
             res.status(500).send('Internal Server Error');
         });
-    //email = req.session.email
-
-
-
-    // Log the result to check if orders are being fetched
-    /* 
-     const result = await orderService.getAllOrdersOfUser();
-    console.log(result.status);
- 
-     if (result.status === 200) {
-         // Render your EJS view and pass the data
-         return res.render('orders', { orders: result.data });
-     } else {
-         // If no orders, return an error message in the view
-         console.log("ffffffffffffffffffffffffffffff")
-         return res.status(result.status).json({ message: result.message });
-     }*/
 }
 
 module.exports = {
--- services/order.js
+++ services/order.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose');
 
 async function getAllOrdersOfUser(uid) {
 
+    const uid_as_object = new mongoose.Types.ObjectId(uid);
     //const orders = await Orders.find({ email }); new mongoose.Types.ObjectId('66df5accc1d6b1eae2660677')
-    const orders = await Orders.find({ userId: uid })
+    const orders = await Orders.find({ userId: uid_as_object })
 
-    if (orders.length === 0) {
-        return res.status(404).json({ message: 'No orders found for this user' });
-    }
     // Format the data to include product details from each order
     const formattedOrders = orders.map(order => ({
         orderId: order._id,
@@ -21,7 +19,6 @@ async function getAllOrdersOfUser(uid) {
             quantity: item.quantity
         }))
     }));
-
     return formattedOrders;
 
 
