fbc010a0fd03b8e4b72c0c282d3d3796a950b2fc
Yaaroosh <ydoolman@gmail.com>
2024-10-03 00:55:30 +0300

    admin dashboard option only shows for admins now

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -13,13 +13,13 @@ router.get("/dashboard", isAdmin, (req, res) => {
 router.get('/products', isAdmin, adminController.getAllProducts);
 
 // POST route to add a new product
-router.post('/products/add', adminController.addProduct); // Add new product
+router.post('/products/add', isAdmin,adminController.addProduct); // Add new product
 // Route to edit a product
-router.put('/products/edit', adminController.editProduct);
+router.put('/products/edit',isAdmin, adminController.editProduct);
 
 
 // Route to delete a product
-router.delete('/products/delete', adminController.deleteProduct);
+router.delete('/products/delete', isAdmin,adminController.deleteProduct);
 
 
 module.exports = router;
